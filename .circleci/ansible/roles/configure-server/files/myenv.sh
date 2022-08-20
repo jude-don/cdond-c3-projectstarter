94ed639cc77efae8514c66c0fb28ac6f4c040ab4
jude-don <judedonkor75@gmail.com>
2022-08-20 15:39:05 +0000

    Update myenv.sh

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -2,8 +2,8 @@ export TYPEORM CONNECTION=postgres
 export TYPEORM ENTITIES=./src/modules/domain/**/*.entity.ts
 export TYPEORMHOST=postgres.cnsuxntldycw.us-east-1.rds.amazonaws.com
 export TYPEORM PORT=5432
-export TYPEORM USERNAME-postgres
-export TYPEORM PASSWORD-postgres
-export TYPEORM DATABASE-postgres
+export TYPEORM USERNAME=postgres
+export TYPEORM PASSWORD=postgres
+export TYPEORM DATABASE=postgres
 export TYPEORM MIGRATIONS=./src/migrations/*.ts
 export TYPEORM MIGRATIONS DIR=./src/migrations
